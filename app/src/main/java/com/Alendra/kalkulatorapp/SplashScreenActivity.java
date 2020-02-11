722af5123f71063b4215825b29ecd28df416e7ac
Alendra <47442771+OpsAhmad@users.noreply.github.com>
2020-02-11 17:45:30 +0700

    Initial commit

---
+++ app/src/main/java/com/Alendra/kalkulatorapp/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.Alendra.kalkulatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //untuk menghilangkan action Bar
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
